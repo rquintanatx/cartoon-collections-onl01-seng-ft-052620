d17a20ca9ea3646591fcd64ed32ba456b9cf2c26
Robert Quintana <robertquintanatx@gmail.com>
2020-05-29 06:47:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarf_array)
+  dwarf_number = 1
+  dwarf_array.each do |dwarf| 
+    puts "#{dwarf_number}. #{dwarf}"
+    dwarf_number += 1
+  end  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(call_array)
+  mod_call_array = call_array.collect do |call|
+    call.capitalize + "!"
+  end   
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call_array)
+  call_array.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredient_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredient_array.find do |ingredient|
+    if cheese_types.include?(ingredient)
+      return ingredient
+    else 
+    end
+  end
 end
+
+find_the_cheese(["tomato soup", "cheddar", "oyster crackers", "gouda"])
+
