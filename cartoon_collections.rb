46d724ab536bb9fa812b48dacfa0ee87979766d1
Robert Quintana <robertquintanatx@gmail.com>
2020-05-29 06:43:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,14 +23,11 @@ end
 def find_the_cheese(ingredient_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   ingredient_array.find do |ingredient|
-    i = 0 
-    while i < 2
-      if cheese_types[i] == ingredient
-        return ingredient
-      else 
-        return nil
-      end
-     end
-  end   
+    if cheese_types.include?(ingredient)
+      return ingredient
+    else 
+      return nil
+    end
+  end
 end
 
