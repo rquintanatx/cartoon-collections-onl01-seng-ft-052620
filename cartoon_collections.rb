4af6db1133d5ef87345293680c69efed224197b0
Robert Quintana <robertquintanatx@gmail.com>
2020-05-29 06:22:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,9 +20,8 @@ def long_planeteer_calls(call_array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredient_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if
 end
 
-summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
