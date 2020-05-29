9b7accc5db7cdf372207413c7015e4a8bc8f2898
Robert Quintana <robertquintanatx@gmail.com>
2020-05-29 06:09:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,4 +25,4 @@ def find_the_cheese# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
 
-summon_captain_planet()
+summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
